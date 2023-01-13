09cdc229942763bdfdb2b22be0201224039a3be8
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-01-13 17:22:48 +0100

    u

--- get_iso.sh
+++ get_iso.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 wget -c --retry-connrefused --tries=0 --timeout=5 https://raw.githubusercontent.com/SaracenRhue/isoDownloader/main/main.py
-python3 main.py
+python main.py
 rm -fr main.py
