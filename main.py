a2ea3eeb5122dde1d4200acf5708fd105bcf5a9a
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-01-13 18:08:01 +0100

    refactoring

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def get_arch_amd64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso'):
-            file = str(link.get('href'))
+            iso_urls['amd64']['arch'] = file_dir + str(link.get('href'))
             break
-    iso_urls['amd64']['arch'] = file_dir + file
+
+def get_debian_amd64():
+    iso_urls['amd64']['debian'] = 'https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/weekly-builds/amd64/iso-cd/firmware-testing-amd64-netinst.iso'
 
 def get_endeavouros_amd64():
     URL = 'https://endeavouros.com/latest-release/'
@@ -34,9 +36,8 @@ def get_endeavouros_amd64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso'):
-            file = str(link.get('href'))
-    iso_urls['amd64']['endeavouros'] = URL + file
-
+            iso_urls['amd64']['endeavouros'] = URL + str(link.get('href'))
+ 
 def get_fedora_amd64():
     URL = 'https://getfedora.org/en/server/download/'
     r = requests.get(URL)
@@ -44,9 +45,8 @@ def get_fedora_amd64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso') and 'netinst' in str(link.get('href')):
-            file = str(link.get('href'))
+            iso_urls['amd64']['fedora'] = str(link.get('href'))
             break
-    iso_urls['amd64']['fedora'] = file
 
 def get_kali_amd64():
     URL = 'https://www.kali.org/get-kali/#kali-installer-images'
@@ -55,9 +55,8 @@ def get_kali_amd64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso') and 'netinst' in str(link.get('href')) and 'amd64' in str(link.get('href')):
-            file = str(link.get('href'))
+            iso_urls['amd64']['kali'] = str(link.get('href'))
             break
-    iso_urls['amd64']['kali'] = file
 
 def get_nixos_amd64():
     URL = 'https://nixos.org/download.html'
@@ -66,9 +65,8 @@ def get_nixos_amd64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso') and 'gnome-x86' in str(link.get('href')):
-            file = str(link.get('href'))
+            iso_urls['amd64']['nixos'] = str(link.get('href'))
             break
-    iso_urls['amd64']['nixos'] = file
 
 def get_ubuntu_amd64():
     URL = 'https://ubuntu.com/download/server#architectures'
@@ -77,9 +75,8 @@ def get_ubuntu_amd64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso'):
-            file = str(link.get('href'))
+            iso_urls['amd64']['ubuntu'] = str(link.get('href'))
             break
-    iso_urls['amd64']['ubuntu'] = file
 
 # arm64
 def get_debian_arm64():
@@ -89,9 +86,8 @@ def get_debian_arm64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso'): 
-            file = str(link.get('href'))
+            iso_urls['arm64']['debian'] = URL + str(link.get('href'))
             break
-    iso_urls['arm64']['debian'] = URL + file
 
 def get_fedora_arm64():
     URL = 'https://getfedora.org/en/server/download/'
@@ -100,9 +96,8 @@ def get_fedora_arm64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso') and 'netinst' in str(link.get('href')) and 'aarch64' in str(link.get('href')):
-            file = str(link.get('href'))
+            iso_urls['arm64']['fedora'] = str(link.get('href'))
             break
-    iso_urls['arm64']['fedora'] = file
 
 def get_kali_arm64():
     URL = 'https://www.kali.org/get-kali/#kali-installer-images'
@@ -111,9 +106,8 @@ def get_kali_arm64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso') and 'netinst' in str(link.get('href')) and 'arm64' in str(link.get('href')):
-            file = str(link.get('href'))
+            iso_urls['arm64']['kali'] = str(link.get('href'))
             break
-    iso_urls['arm64']['kali'] = file
 
 def get_nixos_arm64():
     URL = 'https://nixos.org/download.html'
@@ -122,16 +116,13 @@ def get_nixos_arm64():
     links = soup.find_all('a')
     for link in links:
         if str(link.get('href')).endswith('.iso') and 'gnome-aarch64' in str(link.get('href')):
-            file = str(link.get('href'))
+            iso_urls['arm64']['nixos'] = str(link.get('href'))
             break
-    iso_urls['arm64']['nixos'] = file
-
-
 
 iso_urls = {
     'amd64':{
         'arch': '',
-        'debian': 'https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/weekly-builds/amd64/iso-cd/firmware-testing-amd64-netinst.iso',
+        'debian': '',
         'endeavouros': '',
         'fedora': '',
         'kali': '',
@@ -148,16 +139,17 @@ iso_urls = {
 title = 'Please choose your architecture: '
 options = ['amd64', 'arm64']
 option, index = pick(options, title)
+
 if option == 'amd64':
     title = 'Please choose your OS: '
     options = list(iso_urls['amd64'].keys())
     selected = pick(options, title, multiselect=True, min_selection_count=1)
-    items = [i[0] for i in selected]
-    for i in items:
+    for i in [i[0] for i in selected]:
         if i == 'arch':
             get_arch_amd64()
             download_file(iso_urls[option]['arch'])
         elif i == 'debian':
+            get_debian_amd64()
             download_file(iso_urls[option]['debian'])
         elif i == 'endeavouros':
             get_endeavouros_amd64()
@@ -176,8 +168,7 @@ elif option == 'arm64':
     title = 'Please choose your OS: '
     options = list(iso_urls['arm64'].keys())
     selected = pick(options, title, multiselect=True, min_selection_count=1)
-    items = [i[0] for i in selected]
-    for i in items:
+    for i in [i[0] for i in selected]:
         if i == 'debian':
             get_debian_arm64()
             download_file(iso_urls[option]['debian'])
